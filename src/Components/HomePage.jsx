4fc5f80fc548603608eeb7ddbe5e8c5b169b099f
Diti Arora <diti.aromatic@gmail.com>
2022-05-23 22:10:39 +0530

    final loader removed

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,17 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Post from './Post'
-import Loader from './Loader';
 
 const HomePage = ({ profileImg, posts, bookmarkPost, setBookmarkPost, onDelete, myPost }) => {
-
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => setTimeout(setLoading, 800, true) , [posts])
 
   return (
     <>
       <div className='flex pt-20 flex-col items-center'>
-        {!loading && <Loader />}
         {
             posts.map(post => <Post key={post.id} bookmarkPost={bookmarkPost} setBookmarkPost={setBookmarkPost} onDelete={onDelete} post={post} img={profileImg} myPost={myPost} />)
           }
